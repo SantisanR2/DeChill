3d8e4e9c2835ecdcac2ff3153db262a49c6179b7
garasnote <spawn4102@gmail.com>
2023-03-06 19:54:16 -0500

    Le hice un cambio xd

--- PruebaJP.py
+++ PruebaJP.py
@@ -5,22 +5,22 @@ def main():
     suma1 = 0
     
     for i in range(max+1):
-        suma1 = suma1 + i
+        suma1 += i
     print("Por el primer for da: " + str(suma1))
     
     cont = 0
     suma2 = 0
     
     while(cont <= max):
-        suma2 = suma2 + cont
-        cont = cont + 1
+        suma2 += cont
+        cont += 1
     print("Usando el while da: " + str(suma2))
     
     natu = list(range(0,max+1))
     suma3 = 0
     
     for i in natu:
-        suma3 = suma3 + i
+        suma3 += i
     print("Usando for each da: " + str(suma3))
     
 main()
