9d8cd2575183ed5888ad2ce352d293f39096c1f5
garasnote <spawn4102@gmail.com>
2023-03-06 19:51:06 -0500

    Prueba de Juan Pablo XD

---
+++ PruebaJP.py
@@ -0,0 +1,26 @@
+
+def main():
+    #Número máximo
+    max = 100
+    suma1 = 0
+    
+    for i in range(max+1):
+        suma1 = suma1 + i
+    print("Por el primer for da: " + str(suma1))
+    
+    cont = 0
+    suma2 = 0
+    
+    while(cont <= max):
+        suma2 = suma2 + cont
+        cont = cont + 1
+    print("Usando el while da: " + str(suma2))
+    
+    natu = list(range(0,max+1))
+    suma3 = 0
+    
+    for i in natu:
+        suma3 = suma3 + i
+    print("Usando for each da: " + str(suma3))
+    
+main()
